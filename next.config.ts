d493bbb179ca12dd61dcf974fd9eb00fd06ce836
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2026-05-19 23:33:04 +0330

    c

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 import Icons from "unplugin-icons/webpack";
 
+const basePath =
+  process.env.NEXT_PUBLIC_BASE_PATH || process.env.BUILD_LOCALE
+    ? "/" + process.env.BUILD_LOCALE
+    : "";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath,
   allowedDevOrigins: ["172.23.168.73"],
   images: {
     unoptimized: true,
